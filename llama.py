6ee63a07af1184a876a79d87082d983dffa9defa
elba <1229953662@qq.com>
2024-10-24 08:50:02 +0800

    Update llama.py

--- llama.py
+++ llama.py
@@ -21,18 +21,35 @@ def repeat_kv(x: torch.Tensor, n_rep: int) -> torch.Tensor:
     )
 
 class LLAMA(nn.Module):
-    def __init__(self,in_dim,in_frame,hidden_dim,n_heads,num_layer=2):
+    def __init__(self,in_dim,in_frame,hidden_dim,n_heads,out_dim,num_ik=4,num_layer=2):
         super(LLAMA,self).__init__()
         self.freqs_cis=precompute_freqs_cis(hidden_dim*n_heads,in_dim)
+        self.spa_freqs_cis=precompute_freqs_cis(hidden_dim*n_heads,hidden_dim*n_heads)
         self.time_norm=RMSNorm(hidden_dim*n_heads)
-        self.in_proj=nn.Linear(in_frame,hidden_dim*n_heads)
-        self.model = []
+        self.time_in_proj=nn.Linear(in_frame,hidden_dim*n_heads)
+        self.spa_in_proj=nn.Linear(in_dim,hidden_dim*n_heads)
+        self.time_model = []
+        self.spa_model=[]
         for i in range(0,num_layer):
-            self.model.append(torch.nn.Sequential(Res_attention(hidden_dim*n_heads,1,hidden_dim*n_heads,self.freqs_cis)))
-        self.model=torch.nn.ModuleList(self.model)
+            self.time_model.append(torch.nn.Sequential(Res_attention(hidden_dim*n_heads,1,hidden_dim*n_heads,self.freqs_cis)))
+            self.spa_model.append(torch.nn.Sequential(Res_attention(hidden_dim*n_heads,1,hidden_dim*n_heads,self.spa_freqs_cis)))
+        self.time_model=torch.nn.ModuleList(self.time_model)
+        self.spa_model=torch.nn.ModuleList(self.spa_model)
+        
+        self.spa_out_proj=nn.Linear(hidden_dim*n_heads,out_dim)
+        self.time_out_proj=nn.Linear(hidden_dim*n_heads,in_frame)
+        
+        self.ik=RealNVP(num_ik,out_dim,out_dim)
     def forward(self,x):
-        x=self.in_proj(x)
-        for layer in self.model:
+        x=self.time_in_proj(x)
+        for layer in self.time_model:
             x=layer(x)
-        return x
+        x=self.spa_in_proj(x.permute(0,2,1))
+        for layer in self.spa_model:
+            x=layer(x)
+        x=self.spa_out_proj(x)
+        x=self.time_out_proj(x.permute(0,2,1))
+        rot=self.ik(x.permute(0,2,1))[0].permute(0,2,1)
+        return x,rot
+ 
  
